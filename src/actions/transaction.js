fd3a107a822686bbfeffe116fec077ca4e6720b3
HadiAlizade77 <alizadehadi08@gmail.com>
2020-01-25 18:54:06 +0200

    added configuration reducer

---
+++ src/actions/transaction.js
@@ -0,0 +1,19 @@
+import {
+  ADD_TRANSACTION,
+  REMOVE_TRANSACTION,
+} from './constants';
+
+export const addTransaction = ({ name, amount }) => ({
+  type: ADD_TRANSACTION,
+  payload: {
+    name,
+    amount,
+  },
+});
+
+export const removeTransaction = ({ name }) => ({
+  type: REMOVE_TRANSACTION,
+  payload: {
+    name,
+  },
+});
