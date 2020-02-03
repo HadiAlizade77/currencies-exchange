bc5e5a60aacea6dcfe89fcb5b3d8be3b074909c1
HadiAlizade77 <alizadehadi08@gmail.com>
2020-02-03 13:21:11 +0200

    added selectors tests

---
+++ src/selectors/transaction.js
@@ -0,0 +1,17 @@
+export const getTransactions = ({ transactions }) => transactions;
+
+export const getBiggestTransaction = _.compose(
+  _.maxBy('amount'),
+  getTransactions,
+);
+
+export const getTransactionSum = _.compose(
+  _.sumBy('amount'),
+  getTransactions,
+);
+
+export const isTransactionNameDuplicated = (state) => (name) => _.compose(
+  Boolean,
+  _.find({ name }),
+  getTransactions,
+)(state);
